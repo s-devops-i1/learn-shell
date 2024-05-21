1fbecb5658a13a445fb5dcc18dde2c1c83a0347f
shuja <shuja.hafez@gmail.com>
2024-05-21 18:42:25 +0530

    print func added and cp order changed

--- backend.sh
+++ backend.sh
@@ -1,29 +1,122 @@
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 dnf module disable nodejs -y &>>/tmp/expense.log
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 dnf install nodejs -y &>>/tmp/expense.log
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 useradd expense
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 mkdir /app
-echo $?
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
-echo $?
-cd /app
-echo $?
-unzip /tmp/backend.zip &>>/tmp/expense.log
-echo $?
-npm install
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 cp backend.service /etc/systemd/system/backend.service
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
+cd /app
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
+unzip /tmp/backend.zip &>>/tmp/expense.log
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
+npm install
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
+
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 systemctl daemon-reload
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 systemctl enable backend
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 systemctl start backend
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 dnf install mysql -y
-echo $?
+print_task_heading(){
+  if [ $? = 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+     echo -e "\e[32mFailure\e[0m"
+  fi
+}
 mysql -h 172.31.45.99 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 

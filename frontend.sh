d28b7a3ab595ad317cfb4004a1c7ce1f1dbdaba1
shuja <shuja.hafez@gmail.com>
2024-05-21 17:39:56 +0530

    removed frontend.zip from tmp

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,7 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 cp expense.conf /etc/nginx/default.d/expense.conf
+rm -rf /tmp/frontend.zip
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 unzip /tmp/frontend.zip
 cd /usr/share/nginx/html

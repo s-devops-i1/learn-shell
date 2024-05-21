9c241fc8f16c79c73c455a371e6a9404ea7b5b4f
shuja <shuja.hafez@gmail.com>
2024-05-21 17:37:12 +0530

    changed order of unzip

--- frontend.sh
+++ frontend.sh
@@ -4,8 +4,8 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 cp expense.conf /etc/nginx/default.d/expense.conf
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+cd /usr/share/nginx/html
 
 systemctl restart nginx
 

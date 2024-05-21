723a6688c087137496efec5b4d89a31954805486
shuja <shuja.hafez@gmail.com>
2024-05-21 17:57:10 +0530

    cd order changed

--- frontend.sh
+++ frontend.sh
@@ -4,9 +4,10 @@ systemctl start nginx
 
 cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /tmp/frontend.zip
-cd /usr/share/nginx/html
+
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 frnt_end_file=/tmp/frontend.zip
+cd /usr/share/nginx/html/
 if [ -s "${frnt_end_file}"  ]; then
   echo "frontend contents downloaded"
   else

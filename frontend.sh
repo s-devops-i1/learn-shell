554583cfa7d2b5e32e3c97b9a5665e2e68cc69b2
shuja <shuja.hafez@gmail.com>
2024-05-21 17:50:20 +0530

    cd location changed

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,7 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /tmp/frontend.zip
+cd /usr/share/nginx/html
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 frnt_end_file=/tmp/frontend.zip
 if [ -s "${frnt_end_file}"  ]; then
@@ -12,7 +13,7 @@ if [ -s "${frnt_end_file}"  ]; then
    unzip /tmp/frontend.zip
 fi
 
-cd /usr/share/nginx/html
+
 
 systemctl restart nginx
 

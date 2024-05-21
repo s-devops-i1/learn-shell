c4b2c4a06f099900b83bac74b6a7cd4254ac2323
shuja <shuja.hafez@gmail.com>
2024-05-21 17:34:57 +0530

    changed order of cp

--- frontend.sh
+++ frontend.sh
@@ -2,9 +2,10 @@ dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
+cp expense.conf /etc/nginx/default.d/expense.conf
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl restart nginx
 

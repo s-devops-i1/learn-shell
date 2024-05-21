9c2b26083440696acb20d92b51ad704db036396d
shuja <shuja.hafez@gmail.com>
2024-05-21 18:06:17 +0530

    $ fixed

--- frontend.sh
+++ frontend.sh
@@ -20,13 +20,13 @@ fi
 systemctl restart nginx
 EOF
 
-dnf install nginx -y  &>>$/tmp/expense.log
-systemctl enable nginx &>>$/tmp/expense.log
+dnf install nginx -y  &>>/tmp/expense.log
+systemctl enable nginx &>>/tmp/expense.log
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$/tmp/expense.log
-systemctl restart nginx &>>$/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
 
 

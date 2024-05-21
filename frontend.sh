51fd8a1c85a901cd2d28fcea1d520fc06cd72d43
shuja <shuja.hafez@gmail.com>
2024-05-21 18:02:01 +0530

    testing old code

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+<<EOF
 dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
@@ -17,4 +18,14 @@ fi
 
 
 systemctl restart nginx
+EOF
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+
 
